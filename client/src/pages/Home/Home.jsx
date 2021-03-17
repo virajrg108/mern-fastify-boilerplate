927d9ad14a8f08775be668148972f576120e8e0e
virajrg108 <vrgawde_b16@et.vjti.ac.in>
2021-03-17 12:15:51 +0530

    Redux support added

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { withRouter } from "react-router-dom"
+
+const mapStateToProps = state => {
+  return {
+    count: state
+  };
+};
+const mapDispatchToProps = dispatch => {
+  return {
+    handleIncrementClick: () => dispatch({ type: 'INCREMENT' }),
+    handleDecrementClick: () => dispatch({type: 'DECREMENT'})
+  }
+};
 
 class Home extends React.Component {
   constructor(props) {
@@ -13,9 +26,11 @@ class Home extends React.Component {
     return (
       <div>Home
         <button onClick={this.gotoLogin}>Go to login</button>
+        {this.props.count}
+        <button onClick={this.props.handleIncrementClick}>Inc</button>
       </div>
     )
   }
 }
 
-export default withRouter(Home);
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(Home));
